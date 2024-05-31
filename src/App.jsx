2d57d50396193f3a4003eea7e354ebcae5f1a876
AhnEunJin <eunjin.an@datastreams.co.kr>
2024-05-31 19:13:03 +0900

    router setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAll from './pages/ProductAll';
+import Login from './pages/Login';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
-  return <div></div>;
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<ProductAll />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/product/:id' element={<ProductDetail />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/ProductAll.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductAll() {
+    return (
+        <div>
+            <h1>전체 상품 페이지</h1>
+        </div>
+    );
+}
