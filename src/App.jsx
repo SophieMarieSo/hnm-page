e934728825e5b52c0caab6ec0260aede3ca5dea5
AhnEunJin <modic970515@gmail.com>
2024-06-02 23:30:53 +0900

    private route로 상품 상세 페이지 보호

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import ProductAll from './pages/ProductAll';
 import Login from './pages/Login';
-import ProductDetail from './pages/ProductDetail';
 import Navbar from './component/Navbar';
 import { useState } from 'react';
+import PrivateRoute from './route/PrivateRoute';
 
 function App() {
-    const [auth, setAuth] = useState(true);
-    return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<ProductAll />} />
-                <Route path='/login' element={<Login setAuth={setAuth} />} />
-                <Route path='/product/:id' element={<ProductDetail />} />
-            </Routes>
-        </div>
-    );
+  const [auth, setAuth] = useState(false);
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<ProductAll />} />
+        <Route path='/login' element={<Login setAuth={setAuth} />} />
+        <Route path='/product/:id' element={<PrivateRoute auth={auth} />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductCard({ product }) {
-    return (
-        <div className='product-card'>
-            <img src={product?.img} alt='' />
-            <div className={product?.new.toString() && 'new-product'}>
-                {product?.new ? '🎉신제품🎉' : null}
-            </div>
-            <div>{product?.choice ? 'Concious Choice' : null}</div>
-            <h5>{product?.title}</h5>
-            <div className='product-price'>₩ {product?.price}</div>
-        </div>
-    );
+  const { id } = product;
+  const navigate = useNavigate();
+  return (
+    <div
+      className='product-card'
+      onClick={() => {
+        navigate(`/product/${id}`);
+      }}
+    >
+      <img src={product?.img} alt='' />
+      <div className={product?.new.toString() && 'new-product'}>
+        {product?.new ? '🎉신제품🎉' : null}
+      </div>
+      <div>{product?.choice ? 'Concious Choice' : null}</div>
+      <h5>{product?.title}</h5>
+      <div className='product-price'>₩ {product?.price}</div>
+    </div>
+  );
 }
