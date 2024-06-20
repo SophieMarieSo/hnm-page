a2e4f59fa78cbeb51cb88a4f344d1ec2e4396842
AhnEunJin <eunjin.an@datastreams.co.kr>
2024-06-20 18:58:41 +0900

    개발 생산성을 높여줄 redux-devtools 설치

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import productReducer from './reducers/productReducer';
 import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(productReducer, applyMiddleware(thunk));
+const store = createStore(
+    productReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
